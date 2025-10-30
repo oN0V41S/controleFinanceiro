952f898ea6afbd262cd1f40359710724208dba14
on0v41s <129956152+oN0V41S@users.noreply.github.com>
2025-10-30 15:38:39 +0000

    error: project is not with CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
-// src/app/layout.tsx
 import type { Metadata } from "next";
-import "./globals.css"; // Você precisará criar este arquivo para o Tailwind
+import "./global.css"; // Você precisará criar este arquivo para o Tailwind
 
 export const metadata: Metadata = {
   title: "Controle Financeiro",
